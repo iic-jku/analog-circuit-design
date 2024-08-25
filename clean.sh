149dbb43ba44ad09b69a974d8462a0cc330f6807
Harald Pretl <harald.pretl@jku.at>
2024-08-25 11:29:51 +0200

    Add header

--- clean.sh
+++ clean.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# -------------------------------------------------
+# Clean all auxiliary files from the render process
+# -------------------------------------------------
+# SPDX-FileCopyrightText: 2024 Harald Pretl
+# Johannes Kepler University, Institute for Integrated Circuits
+# SPDX-License-Identifier: Apache-2.0
+
 rm -rf _freeze
 rm -rf _manuscript
 rm -rf *.html
