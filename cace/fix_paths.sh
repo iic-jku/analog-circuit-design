e83666e32d78443c22cbb26e7bff037648b6f8d8
Harald Pretl <harald.pretl@jku.at>
2024-08-26 15:35:59 +0200

    Rename script, remove Unicode

--- cace/fix_paths.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-FILE=_docs/ota-5t_schematic.md
-cp $FILE $FILE.tmp
-sed 's#/ota#/cace/_docs/ota#g' $FILE.tmp > $FILE
-rm $FILE.tmp
---
+++ cace/fix_report_md.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILE=_docs/ota-5t_schematic.md
+cp $FILE $FILE.tmp
+# fix paths for Quarto rendering
+sed 's#/ota#/cace/_docs/ota#g' $FILE.tmp > $FILE
+cp $FILE $FILE.tmp
+# remove Unicode for Latex rendering
+iconv -c -f utf-8 -t ascii $FILE.tmp > $FILE
+rm $FILE.tmp
