5cd96eb48cb581252aab933c2cc88554e9bccec4
Harald Pretl <harald.pretl@jku.at>
2024-09-03 12:40:42 +0200

    Rename script

--- cace/fix_report_md_ota-5t.sh
+++ cace/fix_report_md_ota-5t.sh
@@ -3,7 +3,7 @@ FILE=_docs/ota-5t_schematic.md
 cp $FILE $FILE.tmp
 # fix paths for Quarto rendering
 sed 's#/ota#/cace/_docs/ota#g' $FILE.tmp > $FILE
-cp $FILE $FILE.tmp
+# cp $FILE $FILE.tmp
 # remove Unicode for Latex rendering
 # iconv -c -f utf-8 -t ascii $FILE.tmp > $FILE
 rm $FILE.tmp
