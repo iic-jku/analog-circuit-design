14c7ca32e6bee2c46b5165b293a250747c8e3923
Harald Pretl <harald@pretl.eu>
2025-03-17 07:49:00 +0100

    Use schematic-to-svg tool from IIC-OSIC-TOOLS

--- convertsch2svg.sh
+++ convertsch2svg.sh
@@ -2,8 +2,8 @@
 # ----------------------------------------
 # Batch Xschem schematic to SVG converter)
 # ----------------------------------------
-# SPDX-FileCopyrightText: 2024 Harald Pretl
+# SPDX-FileCopyrightText: 2024-2025 Harald Pretl
 # Johannes Kepler University, Institute for Integrated Circuits
 # SPDX-License-Identifier: Apache-2.0
 
-find xschem -type f -name "*.sch" -exec ./iic-sch2svg.sh {} \;
+find xschem -type f -name "*.sch" -exec iic-sch2svg.sh {} \;
