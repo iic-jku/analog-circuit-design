0d38de090f3fcf9d3dcb859e778b660b90c16f6a
Harald Pretl <harald.pretl@jku.at>
2026-01-24 20:41:27 +0100

    Update copyright years to 2026 across files

--- convertsch2svg.sh
+++ convertsch2svg.sh
@@ -2,7 +2,7 @@
 # ----------------------------------------
 # Batch Xschem schematic to SVG converter)
 # ----------------------------------------
-# SPDX-FileCopyrightText: 2024-2025 Harald Pretl
+# SPDX-FileCopyrightText: 2024-2026 Harald Pretl
 # Johannes Kepler University, Institute for Integrated Circuits
 # SPDX-License-Identifier: Apache-2.0
 
